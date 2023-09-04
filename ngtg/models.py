8033bdf5aec13db971b152ac14709d195981d007
olegPilnik <olegpilnik85@gmail.com>
2023-09-04 15:40:40 +0300

    create and registration models Profile

--- ngtg/models.py
+++ ngtg/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+        user_id = models.PositiveIntegerField(unique=True, 
+                                              verbose_name='user ID telegram')
+        user_name = models.CharField(max_length=30, 
+                                        verbose_name='user_name telegram')
+        
+        full_name = models.CharField(max_length=70,
+                                      verbose_name="Повне ім'я" )
+        
+        def __str__(self):
+            return f'{self.user_id}'
+
+
+        class Meta:
+            verbose_name = 'Профіль'
+            verbose_name_plural = 'Профілі'
+            ordering = ('id', )
