6be7f7ceaa9b7d4e357b24b0f784b262892795a4
olegPilnik <olegpilnik85@gmail.com>
2023-09-04 15:02:56 +0300

    load_dotenv

--- new_generation/settings.py
+++ new_generation/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+import os
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_*p0-q0qh2e_byb#*xaooi35#awokpqq5c+hyg3!c#1(hy+qt)'
+
+load_dotenv('.env')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN', '')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
