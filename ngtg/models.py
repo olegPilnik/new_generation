c27e2a29be085de405803b5e92796cf62fbd32f0
olegPilnik <olegpilnik85@gmail.com>
2023-09-05 15:38:46 +0300

    add func 'add_user_to_database'

--- ngtg/models.py
+++ ngtg/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Profile(models.Model):
         user_id = models.PositiveIntegerField(unique=True, 
                                               verbose_name='user ID telegram')
-        user_name = models.CharField(max_length=30, 
+        user_name = models.CharField(max_length=30, blank=True, null=True,
                                         verbose_name='user_name telegram')
         
-        full_name = models.CharField(max_length=70,
+        full_name = models.CharField(max_length=70, blank=True, null=True,
                                       verbose_name="Повне ім'я" )
         
         def __str__(self):
--- ngtg/tgbot.py
+++ ngtg/tgbot.py
@@ -10,11 +10,32 @@ from ngtg.models import Profile
 #  обьявление переменной бота
 bot = AsyncTeleBot(settings.TELEGRAM_BOT_TOKEN)
 
+@sync_to_async
+def add_user_to_database(user_id, user_name, full_name):
+    """Func to add user in DataBase"""
+    p, created = Profile.objects.update_or_create(
+        user_id=user_id,
+        defaults={
+            'user_name': user_name,
+            'full_name': full_name
+
+        }
+    )
+    return created
 
 @bot.message_handler(commands=['start'])
 async def send_welcome(message):
-    await bot.send_message(message.chat.id, f'Hello {message.from_user.first_name}')
+    """Обработчик команды старт записывает в базу данные о новом профиле"""
+    user_id = message.from_user.id
+    user_name = message.from_user.username
+    full_name = message.from_user.full_name
 
+    if await add_user_to_database(user_id, user_name, full_name):
+        await bot.send_message(message.chat.id, f'Привіт {message.from_user.first_name}, тебе додано до бази')
+    else:
+        await bot.send_message(message.chat.id, f'Привіт {message.from_user.first_name}, ти є у базі')
+        
+      
 
 
 
